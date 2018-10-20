3f9928fb6e5828cd2de1fa4e88004665b70db896
DRON\Sedjj <_m8u9qQMNCTTZEcbFR5pukoZigy22xTNJ63m3UWV_6XAjXYAspq_DdGtYPFotMPW>
2018-10-20 20:31:12 +0500

    Добавил различные достыпы к бд, для более удобной отладки

--- src/utils/dbProvider.js
+++ src/utils/dbProvider.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const log = require('./logger');
 const config = require('config');
 
-const dbUri = config.get('db.uri');
+const dbUri = process.env.NODE_ENV === 'development'
+	? `mongodb://${config.get('dbDev.user')}:${encodeURIComponent(config.get('dbDev.pass'))}@${config.get('dbDev.hostString')}${config.get('dbDev.name')}`
+	: `mongodb://${config.get('dbProd.user')}:${encodeURIComponent(config.get('dbProd.pass'))}@${config.get('dbProd.hostString')}${config.get('dbProd.name')}`;
+
 mongoose.Promise = global.Promise;
 mongoose.connect(dbUri, {
 	useNewUrlParser: true
