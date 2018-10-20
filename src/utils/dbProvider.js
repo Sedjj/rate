6e43d94b16da528acd2bd0786baf8813c77fe351
Sedjj <omeldarl@rambler.ru>
2018-10-20 21:34:49 +0500

    еще раз исправил настройки для деплоя

--- src/utils/dbProvider.js
+++ src/utils/dbProvider.js
@@ -6,17 +6,19 @@ const dbUri = process.env.NODE_ENV === 'development'
 	? `mongodb://${config.get('dbDev.user')}:${encodeURIComponent(config.get('dbDev.pass'))}@${config.get('dbDev.hostString')}${config.get('dbDev.name')}`
 	: `mongodb://${config.get('dbProd.user')}:${encodeURIComponent(config.get('dbProd.pass'))}@${config.get('dbProd.hostString')}${config.get('dbProd.name')}`;
 
+log.info(`db :${dbUri}`);
+
 mongoose.Promise = global.Promise;
 mongoose.connect(dbUri, {
 	useNewUrlParser: true
 });
 const db = mongoose.connection;
 
-db.on('error', function (error) {
-	log.error('Connection error:', error.message);
+db.on('error', (error) => {
+	log.error('Connection error:', error);
 });
 
-db.once('open', function callback() {
+db.once('open', () => {
 	log.info(`Connected to DB on ${dbUri}`);
 });
 
