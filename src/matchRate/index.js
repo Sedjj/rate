91ad67bea844482d7eefd73b3ffd828f36d36d1d
Sedjj <omeldarl@rambler.ru>
2019-06-16 00:08:04 +0500

    исправил косяк

--- src/matchRate/index.js
+++ src/matchRate/index.js
@@ -13,14 +13,14 @@ const typeRate = config.choice.live.football.typeRate;
  * @returns {Promise<void>}
  */
 async function matchRate(statistic, type = '') {
-	console.log(type);
+	// console.log(type);
 	const total = statistic.score.sc1 + statistic.score.sc2 + typeRate[statistic.strategy];
 	const {snapshot, cards, matchId, score} = statistic;
 	switch (statistic.strategy) {
 		case 1 :
 			if (snapshot.start.x >= 3.16 && (snapshot.start.x - snapshot.start.p1) > 0.18) {
 				if (score.sc1 ===0 &&  score.sc2 ===1) {
-					await sendMessageChat(decorateMessageTennis(statistic));
+					// await sendMessageChat(decorateMessageTennis(statistic));
 					await performEmulation(matchId, 9, `Total Over ${total}`);
 				}
 			}
