dbcde4cd0a0c8df07447c5730cb2270bb635f4e5
Sedjj <omeldarl@rambler.ru>
2019-08-02 16:29:26 +0500

    апр

--- src/selenium/bot/mirrorXbet.js
+++ src/selenium/bot/mirrorXbet.js
@@ -70,6 +70,7 @@ async function performEmulation(ids, numberColumn, totalName) {
  */
 async function authorization(driver) {
 	log.debug('Authorization start');
+	await driver.sleep(speed.normal);
 	await screenShot(driver, `Authorization_start${(new Date()).getTime()}.png`);
 	if (!await findIdAndCall(driver, 'curLoginForm')) {
 		if (await findSelectorCss(driver, '.wrap_lk')) {
