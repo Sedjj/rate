9aa078799bd49ef978a33c6a55e6957cb1154a87
Sedjj <omeldarl@rambler.ru>
2019-09-02 19:54:45 +0500

    поправил форматирование для канала

--- src/utils/formateMessage.js
+++ src/utils/formateMessage.js
@@ -1,5 +1,4 @@
 const config = require('config');
-const typeRate = config.choice.live.football.typeRate;
 
 /**
  * Сбрасывает все зарезервированные символы для регулярных выражений.
@@ -41,11 +40,18 @@ function secondsToMinutes(time) {
  * @returns {*}
  */
 function decorateMessageMatch(param, type) {
-	return `Матч: ${param.matchId}; Стр: ${param.strategy}; Счет: ${param.score.sc1}:${param.score.sc2}
+	const {
+		matchId,
+		command: {en: {one, two}},
+		group: {en},
+		strategy,
+		score: {sc1, sc2}
+	} = param;
+	return `Матч: ${matchId}; Стр: ${strategy}; Счет: ${sc1}:${sc2}
 	Вид спорта: ${type}
-	Группа: ${param.group.en}
-	  Команда 1:  ${param.command.en.one}
-	  Команда 2:  ${param.command.en.two}`;
+	Группа: ${en}
+	  Команда 1:  ${one}
+	  Команда 2:  ${two}`;
 }
 
 /**
@@ -56,20 +62,36 @@ function decorateMessageMatch(param, type) {
  * @returns {*}
  */
 function decorateMessageTennis(param, type = '') {
-	return `<b>${param.group.en}</b>\n\n${param.command.en.one}\n${param.command.en.two}\n\n<pre>стр ${param.strategy} / ${param.snapshot.start.p1} / ${param.snapshot.start.x} / ${param.snapshot.start.p2}</pre>`;
+	const {
+		command: {en: {one, two}},
+		strategy,
+		group: {en},
+		snapshot: {start},
+	} = param;
+	const index = `${start.p1} / ${start.x} / ${start.p2}`;
+	return `<b>${en}</b>\n\n${one}\n${two}\n\n<pre>стр ${strategy} / ${index}</pre>`;
 }
 
 /**
  * Форматирование строки для канала.
  *
  * @param {Object} param объект матча
+ * @param {String} type вид спорта
  * @returns {string}
  */
-function decorateMessageChannel(param) {
-	const time = secondsToMinutes(param.snapshot.end.time);
-	const scope = `${param.score.sc1}:${param.score.sc2}`;
-	const typeTotal = param.score.sc1 + param.score.sc2 + typeRate[param.strategy];
-	return `<b>${param.group.en}</b>\n\n${param.command.en.one}\n${param.command.en.two}\n\n<pre>${scope} / ${time}'\nTotal over ${typeTotal} / ${param.total}</pre>`;
+function decorateMessageChannel(param, type = '') {
+	const {
+		matchId,
+		command: {en: {one, two}},
+		group: {en},
+		snapshot: {start: {p1, x, p2}, end},
+		score: {sc1, sc2}
+	} = param;
+	const time = secondsToMinutes(end.time);
+	const scope = `${sc1}:${sc2}`;
+	const index = `${p1} / ${x} / ${p2}`;
+	const difference = `${x - p1} / ${x - p2} / ${p2 - p1}`;
+	return `${matchId}\n<b>${en}</b>\n\n${one}\n${two}\n\n<pre>${scope} / ${time}'\n${index}\n${difference}</pre>`;
 }
 
 module.exports = {
