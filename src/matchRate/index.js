3ac8ca500b6eb171b7e593c3386230d6970afd98
Sedjj <omeldarl@rambler.ru>
2019-09-08 23:48:03 +0500

    поменял фильтры

--- src/matchRate/index.js
+++ src/matchRate/index.js
@@ -16,7 +16,6 @@ async function matchRate(statistic, type = '') {
 	const totalRate = statistic.score.sc1 + statistic.score.sc2 + typeRate[statistic.strategy];
 	const {
 		snapshot: {start: {time, x, p1, p2}},
-		group:{en},
 		cards: {before: {one, two}},
 		score: {sc1, sc2},
 		matchId,
@@ -25,37 +24,49 @@ async function matchRate(statistic, type = '') {
 	switch (statistic.strategy) {
 		case 3 :
 			if (limited === 0 && women === 0 && youth === 0) {
-				if(3.85 < x && time < 1200){
-					if (sc1 === 1 && sc2 === 0) {
-						if (0.6 < (x - p1) && 1.45 < (x - p2)) {
-							if(en.toString().trim() !== 'Club Friendlies'.trim()){
-								await sendMessageChat(decorateMessageChannel(statistic, type));
-								await performEmulation(matchId, 9, `Total Over ${totalRate}`);
-							}
-						}
+				if (sc1 === 1 && sc2 === 0) {
+					if (0.15 < (x - p1) && 0.99 < (x - p2) && (p2 - p1) < 0.25) {
+						await performEmulation(matchId, 9, `Total Over ${totalRate}`);
 					}
-					if (sc1 === 0 && sc2 === 1) {
-						if (two.attacks < 10 && two.danAttacks < 5 && 252 < time) {
-							await sendMessageChat(decorateMessageChannel(statistic, type));
-							await performEmulation(matchId, 9, `Total Over ${totalRate}`);
-						}
+				}
+				if (sc1 === 0 && sc2 === 1) {
+					if (1.14 < (x - p1) && 1.4 < (x - p2) && two.attacks < 10) {
+						await performEmulation(matchId, 9, `Total Over ${totalRate}`);
 					}
 				}
 			}
 			break;
 		case 7 :
 			if (limited === 0 && women === 0 && youth === 0) {
+				// copy 3 strategy
 				if (sc1 === 1 && sc2 === 0) {
-					if (0.15 <= (x - p1) && (p2 - p1) < 0.25) {
+					if (0.15 < (x - p1) && 0.99 < (x - p2) && (p2 - p1) < 0.25) {
 						await sendMessageChannel(decorateMessageChannel(statistic, type));
 						await sendMessageChannel('Result');
 					}
 				}
 				if (sc1 === 0 && sc2 === 1) {
-					if (one.attacks < 21 && time < 1200) {
-						if (0.8 < (x - p1) && 1.1 < (x - p2)) {
-							await sendMessageChannel(decorateMessageChannel(statistic, type));
-							await sendMessageChannel('Result');
+					if (1.14 < (x - p1) && 1.4 < (x - p2) && two.attacks < 10) {
+						await sendMessageChannel(decorateMessageChannel(statistic, type));
+						await sendMessageChannel('Result');
+					}
+				}
+				// strategy 7
+				if (sc1 === 1 && sc2 === 0) {
+					if ((time < 1800) && (4 < x)) {
+						if (-1.4 <= (p2 - p1) && (p2 - p1) <= 0.8) {
+							if (1.85 < (x - p2)) {
+								await sendMessageChat(decorateMessageChannel(statistic, type));
+							}
+						}
+					}
+				}
+				if (sc1 === 0 && sc2 === 1) {
+					if ((time < 540) && (3.8 < x)) {
+						if (-1.25 < (p2 - p1) && 1 < (x - p1)) {
+							if (0 < one.danAttacks && one.attacks < 10) {
+								await sendMessageChat(decorateMessageChannel(statistic, type));
+							}
 						}
 					}
 				}
